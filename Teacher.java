3ee4e199e309e808ac09ef883c50e31059e8601b
116dariya <116dariya@gmail.com>
2017-11-22 11:16:58 +0600

    Update Teacher.java

--- Teacher.java
+++ Teacher.java
@@ -1,49 +1,33 @@
-/**
- */
+
 public class Teacher extends Employee {
-    /**
-     */
+   
     private DEGREE degree;
 
-    /**
-     */
+    
     private Vector<HashMap<Course, Vector<Students>>> courses;
 
-    /**
-     */
+   
     public void addFolder() {
+       
     }
 
-    /**
-     * @param student 
-     * @param mark 
-     */
+   
     public void putMark(Student student, Mark mark) {
     }
 
-    /**
-     * @param param1 
-     */
+
     public void viewStudent(Student param1) {
     }
 
-    /**
-     * @return 
-     */
+ 
     public Vector<Course> getCourses() {
         return null;
     }
 
-    /**
-     * @param courses 
-     */
     public void setCourses(Vector<Course> courses) {
     }
 
-    /**
-     * @param param1 
-     * @param param2 
-     */
+
     public void putAttendance(Student param1, ATTENDANCE param2) {
     }
 }
