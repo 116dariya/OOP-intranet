fce844133447b7b75dad345f7e78cf3ed5c64cc0
116dariya <116dariya@gmail.com>
2017-11-22 11:06:27 +0600

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,98 +1,61 @@
 import java.lang.*;
 
-/**
- */
 public class Student extends Person {
-    /**
-     */
+   
     private double gpa;
-
-    /**
-     */
     private String ID;
-
-    /**
-     */
     private String intranetLogin;
-
-    /**
-     */
     private DEGREE LevelOfEducation;
-
-    /**
-     */
     private HashMap<Course, MarkAttendance> attestation;
-
-    /**
-     */
     private DATABASE Schedule;
-
-    /**
-     * @param course 
-     */
+    
     public void registerForCourse(Course course) {
+        
     }
 
-    /**
-     */
     public void viewTranscript() {
+        
     }
 
-    /**
-     * @param course 
-     */
+ 
     public void dropCourse(Course course) {
+        
     }
 
-    /**
-     * @return 
-     */
+
     public String getIntranetLogin() {
-        return null;
+        return intranetLogin;
     }
 
-    /**
-     * @param intranetLogin 
-     */
+
     public void setIntranetLogin(String intranetLogin) {
+        
     }
 
-    /**
-     * @return 
-     */
+
     public double getGpa() {
-        return 0;
+        return gpa;
     }
 
-    /**
-     * @param gpa 
-     */
+ 
     public void setGpa(double gpa) {
+        this.gpa = gpa;
     }
 
-    /**
-     * @return 
-     */
+
     public DEGREE getLevelOfEducation() {
-        return null;
+        return  LevelOfEducation;
     }
 
-    /**
-     * @param LevelOfEducation 
-     */
     public void setLevelOfEducation(DEGREE LevelOfEducation) {
+         this.LevelOfEducation =  LevelOfEducation;
     }
-
-    /**
-     * @return 
-     */
+ 
+    
     public HashMap<Course, MarkAttendance> getAttestation() {
         return null;
     }
 
-    /**
-     * @param attestation 
-     */
     public void setAttestation(HashMap<Course, MarkAttendance> attestation) {
     }
 }
