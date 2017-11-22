03f9f8db45a79b39fccd6cf81198aad86474658b
Maxat Sangerbayev <maxat235@gmail.com>
2017-11-22 11:03:01 +0600

    Update GRADES.java

--- GRADES.java
+++ GRADES.java
@@ -2,49 +2,9 @@ import java.lang.*;
 
 /**
  */
-public class GRADES {
-    /**
-     */
-    private String a = A;
-
-    /**
-     */
-    private String mA = -A;
-
-    /**
-     */
-    private String pB = +B;
-
-    /**
-     */
-    private String b = B;
-
-    /**
-     */
-    private String mB = -B;
-
-    /**
-     */
-    private String pC = +C;
-
-    /**
-     */
-    private String c = C;
-
-    /**
-     */
-    private String mC = -C;
-
-    /**
-     */
-    private String pD = +D;
-
-    /**
-     */
-    private String d = D;
-
-    /**
-     */
-    private String f = F;
+public enum Grades {
+    
+    A, -A, +B, B, -B, 
+    +C, C, -C, +D, D, F
 }
 
